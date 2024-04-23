7144d17c1fba79c6b4379af4c7c425428109203d
kaekoko <kekoko.mm@gmail.com>
2024-04-23 14:07:36 +0630

    app

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const socket = require("socket.io");
 
 /**----app setup---- */
 const app = express();
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
 app.use(express.static('public'));
 
 /**----server setup---- */
